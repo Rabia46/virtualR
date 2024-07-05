922aaf82095f5582af7acd0ca1a996ba2f95abb1
Rabia Hussain <hrabia847@gmail.com>
2024-07-05 15:47:14 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-
-
-import { Menu, X } from "lucide-react";
+ import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo1.png";
 import { navItems } from "../constants";
 
 const Navbar = () => {
@@ -13,12 +11,19 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg      ">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <a href="#home">
+              <img
+                className="h-20 w-20 mr-2 transition-transform duration-300 hover:scale-105"
+                src={logo}
+                alt="Logo"
+              />
+            </a>
+            <span className="text-xl tracking-tight">   </span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
@@ -33,12 +38,15 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 border rounded-md">
+            <a
+              href="#"
+              className="py-2 px-3 border rounded-md transition-transform duration-300 hover:scale-105"
+            >
               Sign In
             </a>
             <a
               href="#"
-              className="bg-gradient-to-r from-green-500 to-pink-800 py-2 px-3 rounded-md"
+              className="bg-gradient-to-r from-green-500 to-pink-800 py-2 px-3 rounded-md transition-transform duration-300 hover:scale-105"
             >
               Create an account
             </a>
@@ -64,12 +72,16 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border rounded-md">
+              <a
+                href="#"
+                className="py-2 px-3 border rounded-md transition-transform duration-300 hover:scale-105"
+              >
                 Sign In
               </a>
               <a
                 href="#"
-                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500
+                 to-orange-800 transition-transform duration-300 hover:scale-105"
               >
                 Create an account
               </a>
